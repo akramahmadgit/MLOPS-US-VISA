7baf0c635dcdfd8c1e59e3a34ad52182a8f7b8bb
Akram Ahmad <akram.ahmad2009@gmail.com>
2024-07-18 16:01:17 +0530

    Demo exception handling completed

--- demo.py
+++ demo.py
@@ -1,3 +1,12 @@
 from us_visa.logger import logging
+from us_visa.exception import USvisaException
+
+import sys
 
 logging.info("You have successfully created a custom logging project")
+
+
+try:
+    a=5/0
+except Exception as e:
+    raise USvisaException(e, sys)
